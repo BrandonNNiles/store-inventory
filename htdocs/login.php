92d2565122a4b38363f96b1e6d6f8cb94db676ec
Brandon Niles <69768591+BrandonNNiles@users.noreply.github.com>
2023-07-19 22:20:37 -0400

    Login failure messages and tracking within session

--- htdocs/login.php
+++ htdocs/login.php
@@ -1,13 +1,29 @@
+<?php
+    //Configurables
+    $title = "CP476 Log In"; //title of the page
+    $postlogin = "postlogin.php"; //postlogin redirection
+
+    //Prevent auth'd users from seeing the login page
+    session_start();
+    if(array_key_exists("auth", $_SESSION)){
+        if($_SESSION["auth"]){
+            header("location:$postlogin"); //redirect authed users
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <link rel="stylesheet" href="stylesheets/login.css">
+        <title><?php echo($title)?></title>
     </head>
     <body>
-        <form method="POST">
-            <div class="login-main">
-                <div class="login-body">
-                    <h1 class="login-title">Log In</h1>
+        <div class="login-main">
+            <div class="login-body">
+                <h1 class="login-title">Log In</h1>
+                <form method="POST">
                     <div class="login-info">
                         <div class="login-entry-cont">
                             <p class="login-entry-text">Username</p>
@@ -24,21 +40,27 @@
                     </div>
                     <div class="submit-cont">
                         <input type="submit" class="login-submit" value="Sign In">
+                        <?php if(array_key_exists("login-fail", $_SESSION)){
+                            if($_SESSION["login-fail"] > 0){ ?>
+                            <div class="error">Previous username/password combination invalid.</div>
+                        <?php }} ?>
                     </div>
-                </div>
+                </form>
             </div>
-        </form>
+        </div>
     </body>
 </html>
 
 <?php
 
-    //Prevent auth'd users from seeing the login page
-    session_start();
-    if(array_key_exists("auth", $_SESSION)){
-        if($_SESSION["auth"]){
-            header("location:postlogin.php"); //redirect unauthed users
-        }
+    //Configurables
+    $title = "CP476 Log In"; //title of the page
+    $postlogin = "postlogin.php"; //postlogin redirection
+
+    if(!array_key_exists("login-fail", $_SESSION)){ //have we failed to login?
+        $_SESSION["login-fail"] = 0;
+    } else {
+        $_SESSION["login-fail"] = $_SESSION["login-fail"] - 1; //no longer fail after an additonal refresh
     }
 
     if($_POST){
@@ -81,8 +103,7 @@
         $row = $result->fetch_assoc();
 
         if($row == NULL){
-            echo("Invalid username");
-            exit();
+            login_failed(); //invalid username
         } elseif($row == false){
             echo("Results failed.");
             exit();
@@ -91,13 +112,18 @@
         $db_pass = $row["user_password"];
 
         if(password_verify($password, $db_pass)) {
-            echo("Login success!");
-            session_start();
             $_SESSION['auth'] = 'true';
             $_SESSION['username'] = $username;
-            header('location:postlogin.php');
+            $_SESSION["login-fail"] = 0;
+            header("location:$postlogin");
         } else {
-            echo("Login failed!");
+            login_failed(); //invalid password
         }
+    }
+
+    function login_failed(){
+        $_SESSION["login-fail"] = 2;
+        header("Refresh:0");
+        exit();
     }
 ?>
