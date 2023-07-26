be5f33ddcbc85bbdbb1fe4e3ee8fb0dfaf316565
Brandon Niles <brandonniles00@gmail.com>
2023-07-26 00:21:06 -0400

    404 placeholder + config upload

---
+++ htdocs/404.php
@@ -0,0 +1,22 @@
+<?php
+    //Configurables
+    $title = "Page Not Found"; //title of the page
+    $require_auth = true; //whether or not the user needs to be logged in to see this page
+    $perm_level = 0; //the user's permision level to see the page (requires require_auth = true)
+
+    require __DIR__ . '/modules/authmanager.php';
+    view_redirect($require_auth, $perm_level);
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="stylesheets/login.css">
+        <title><?php echo($title)?></title>
+    </head>
+    <body>   
+        <p>Sorry, we couldn't find that page!</p>
+        <br>
+        <a href="javascript:history.back()">Go Back</a>
+    </body>
+</html>
