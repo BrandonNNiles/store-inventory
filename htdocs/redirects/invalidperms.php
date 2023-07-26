b8c1eea958c81d3e16ce68fedc53aee58ec9fcb5
Brandon Niles <brandonniles00@gmail.com>
2023-07-26 16:55:57 -0400

    Prettier invalidperms page.

--- htdocs/redirects/invalidperms.php
+++ htdocs/redirects/invalidperms.php
@@ -7,13 +7,24 @@
 <html>
     <head>
         <link rel="stylesheet" href="../stylesheets/login.css">
+        <link rel="stylesheet" href="../stylesheets/redirects.css">
         <title><?php echo($title)?></title>
         <link rel="icon" type="image/png" href="resources/images/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" type="image/png" href="/resources/images/favicon-16x16.png" sizes="16x16" />
     </head>
-    <body>   
-        <p>You have no permissions to view that page.</p>
-        <br>
-        <a href="javascript:history.back()">Go Back</a>
+    <body>
+        <div class="login-main">
+            <div class="login-body">
+                <div class="login-info">   
+                    <h1 class="login-title">You have no permissions to view that page.</h1>
+                    <br>
+                </div>
+                <div class="submit-cont">
+                    <div class ="container">
+                        <a href="javascript:history.back()" class = "back-a">Go Back</a>
+                    </div>  
+                </div>
+            </div>
+        </div>
     </body>
 </html>
